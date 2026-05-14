353050a2a307913f212e9928fc7b8837e6108a70
umar777 <umarbinmuhammed@gmail.com>
2026-05-14 15:22:49 +0100

    test

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,11 @@ import app from "./app.js";
 
 const port = Number(process.env.PORT) || 4000;
 
+// Test route
+app.get("/", (req, res) => {
+  res.send("API Server is running");
+});
+
 app.listen(port, () => {
   console.log(`Backend listening on http://localhost:${port}`);
 });
